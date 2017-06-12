31461ed6e347d3e50d7e847e6eaeb4d19bf46f40
Darcy Clarke <darcy@darcyclarke.me>
2017-06-12 11:06:21 -0400

    Refactor structure

---
+++ scripts/admin.js
@@ -0,0 +1,48 @@
+/* TODO:
+- add/update/remove:
+  - books
+  - links
+  - clients
+  - awards
+  - charities
+  - events
+*/
+const inquirer = require('inquirer')
+const low = require('lowdb')
+const data = require('../data/data.json')
+const db = low()
+db.setState(data)
+
+const argv = require('yargs')
+  .options({
+    'add': {
+      alias: 'a',
+      describe: 'add a new item'
+    }
+  })
+  .argv
+const questions = [
+  {
+    name: 'id',
+    message: 'What\'s the id?'
+  },
+  {
+    name: 'name',
+    message: 'What\'s the name?'
+  },
+  {
+    name: 'author',
+    message: 'Whos\'s the author?'
+  },
+  {
+    name: 'url',
+    message: 'What\'s the url?'
+  }
+]
+
+if (argv.add) {
+  console.log(db.getState())
+  inquirer.prompt([questions]).then(function (answers) {
+
+  })
+}
