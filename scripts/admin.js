73e824d6d5607508738bef5ca612f61309e8d5aa
Darcy Clarke <darcy@darcyclarke.me>
2019-03-18 11:28:59 -0400

    Major changes

--- scripts/admin.js
+++ scripts/admin.js
@@ -41,8 +41,9 @@ const questions = [
 ]
 
 if (argv.add) {
-  console.log(db.getState())
+  // console.log(db.getState())
   inquirer.prompt([questions]).then(function (answers) {
-
+    
   })
-}
+} 
+
