7fb7d2bbe6189973b1ca9d843a4be06ce0a0841a
Darcy Clarke <darcy@darcyclarke.me>
2019-07-15 20:59:04 -0400

    Fix function to serve JSON & allow CORS

--- src/get.js
+++ src/get.js
@@ -2,6 +2,10 @@ const darcy = require('../index')
 exports.handler = function (event, context, callback) {
   callback(null, {
     statusCode: 200,
+    headers: {
+      'content-type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+    },
     body: JSON.stringify(darcy, 2)
   })
 }
--- test/test.js
+++ test/test.js
@@ -1,12 +1,20 @@
-const info = require('../index')
 const assert = require('assert')
+const axios = require('axios')
+const info = require('../index')
 
 describe('require darcy', () => {
-  let info = require('../index')
   it('should return an object', () => {
     assert.equal(typeof info, typeof {})
   })
   it('should have a first name', () => {
     assert.ok(!!info['og:profile']['og:profile:first_name'])
+  })
+})
+
+describe('api', () => {
+  it('should return json', async () => {
+    const json = await axios.get('https://darcy.netlify.com/.netlify/functions/get')
+    console.log(json.headers)
+    assert.equal(typeof json, typeof {})
   })
 })
